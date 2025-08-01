d1f460114b3ebd4b0b20a6bbbcec1000a4c7f9c0
cilolata <cilolata@gmail.com>
2025-08-01 18:54:07 -0300

    refactor: build

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/app.ts',
+    'src/entities/*.ts',
+    'src/lib/**/*.ts',
+    'src/migrations/*.ts'
+  ],
+  outDir: 'dist',
+  format: ['cjs'],
+  dts: true,
+  clean: true,
+  splitting: false,
+  bundle: false,
+  skipNodeModulesBundle: true
+})
