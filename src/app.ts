0a500441828749127f539252bec33935e068c29b
cilolata <cilolata@gmail.com>
2025-08-19 15:40:33 -0300

    feat: install cors

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import "reflect-metadata";
 import "dotenv/config";
-import "./lib/typeorm/typeorm"
+import "./lib/typeorm/typeorm";
 
 import express from "express";
 import { router } from "./routes/routes";
 import { handleError } from "./middlewares/errorHandlers";
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
 import specs from "./config/swagger";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 
 app.use(router);
-app.use(handleError)
+app.use(handleError);
 
-app.use('/postai', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/postai", swaggerUi.serve, swaggerUi.setup(specs));
 
-export default app
+export default app;
